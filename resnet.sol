277554ad34fcff6cd8f5b0cad03a88895e59db62
Sang-Hyeon Park <twodude@naver.com>
2018-08-26 13:59:18 +0900

    Update resnet.sol

--- resnet.sol
+++ resnet.sol
@@ -38,10 +38,13 @@ contract resnet {
     function relu(uint16 W1, uint16 H1, uint16 D1)
     returns(uint16 W2, uint16 H2, uint16 D2) {
         uint8 tmp=0;
+       
+        // Loop num.
+        emit logging(uint32(W1 * H1 * D1));
         
         uint32 i;
         
-        for(i=0; i<W2 * H2 * D2; i++){
+        for(i=0; i<W1 * H1 * D1; i++){
             // max
             if (tmp < 0){
                 tmp = 0;
@@ -50,11 +53,29 @@ contract resnet {
         
         return (W1, H1, D1);
     }
-    
-    // max pool
-    // average pool
-    function pool()
-    returns() {
+
+    // average POOL
+    function pool(uint16 W1, uint16 H1, uint16 D1, uint16 F, uint16 S)
+    returns(uint16 W2, uint16 H2, uint16 D2) {
+        uint16 _W2 = (W1 - F) / S + 1;
+        uint16 _H2 = (H1 - F) / S + 1;
         
-    }
+        // Loop num.
+        emit logging(uint32(_W2 * _H2 * D1 * F * F));
+        
+        uint32 i;
+        uint32 j;
+        
+        for(i=0; i<_W2 * _H2 * D1; i++){
+            uint32 sum=0;
+            
+            for(j=0; j<F * F; j++){
+                sum += (1.5 + 1.5);    
+            }
+            
+            sum /= (F * F);
+        }
+        
+        return (_W2, _H2, D1);
+    }    
 }
